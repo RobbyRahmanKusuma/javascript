4ac7800c1065b9f2e34e7eb027d46d80b9353698
Robby <robbyrahmankusuma@gmail.com>
2021-06-22 04:38:48 +0700

    program sederhana javascript

---
+++ objectKucing.js
@@ -0,0 +1,13 @@
+let kucing = {
+    nama: "Chiko",
+    kelamin: "Laki-laki",
+    jenis: "Anggora",
+    warna: "Belang hitam putih",
+    tinggi: "20cm",
+    kelahiran: "20 Desember 2012",
+};
+
+console.log(kucing.nama);
+console.log(kucing.jenis);
+console.log(kucing.warna);
+console.log(kucing);
